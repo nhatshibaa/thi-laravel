0ad1998c8e754ad445edb08cfa453afb3f0473cf
Your Name <you@example.com>
2021-07-21 15:59:48 +0700

    end

---
+++ database/migrations/2021_07_21_082108_create_apartments_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('apartmentName');
+            $table->string('address');
+            $table->double('price');
+            $table->string('information');
+            $table->string('informationDetail');
+            $table->string('thumbnail');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartments');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+    <meta name="description" content="arillo is responsive html real estate theme">
+    <meta name="author" content="afriq yasin ramadhan">
+
+    <title>Sweet Home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link rel="stylesheet" href="../assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../assets/css/owl.carousel.css">
+    <link rel="stylesheet" href="../assets/css/owl.theme.css">
+    <link rel="stylesheet" href="../assets/css/owl.transitions.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/responsive.css">
+
+    <!--   Google Font  -->
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,600,800,700" rel="stylesheet" type="text/css">
+    <link href='http://fonts.googleapis.com/css?family=Signika:600,400,300' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,500,600' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Philosopher' rel='stylesheet' type='text/css'>
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="../assets/js/html5shiv.js"></script>
+    <script src="../assets/js/respond.min.js"></script>
+    <![endif]-->
+
+
+</head>
+
+<body id="top">
+<section id="navigation">
+    <!-- header begin -->
+    <div class="header">
+        <div class="yamm navbar navbar-default ">
+            <div class="container">
+                <!-- navbar header begin -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.html">
+                        <img alt="Sweet Home Logo" src="../assets/img/Sweet-Home.png"
+                             class="img-responsive center-block">
+                    </a>
+                </div>
+
+                <!-- main navigation begin -->
+                <div class="collapse navbar-collapse navHead pull-right" id="mainMenu">
+
+                    <ul class="nav navbar-nav">
+                        <!-- HOME item: begin -->
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Best Home</a></li>
+                        <li><a href="#">Life Style</a></li>
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="#">Contact Us</a></li>
+                    </ul>
+
+                </div><!-- main navigation FINISH -->
+            </div><!-- container FINISH -->
+        </div><!-- yamm navbar navbar-default FINISH -->
+    </div><!-- header FINISH -->
+</section>
+<section id="service">
+    <!-- begin:content -->
+    <section id="property">
+{{--        <div class="property-wrapper">--}}
+            <div class="container">
+                <!-- begin:latest -->
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="heading-title">
+                            <h2>Newest Home</h2>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <input type="search" class="header heading-title" placeholder="Search...">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="isotope" id="port-items">
+                        <div class="col-md-4 col-sm-6 col-xs-6 element-item sale" data-category="sale">
+                            <div class="property-container">
+                                <div class="property-image">
+
+                                    <img src="assets/img/img02.jpg" alt="arillo real estate theme">
+                                    <div class="property-price">
+                                        <h4>Residential</h4>
+                                        <span>$800,000</span>
+                                    </div>
+                                </div>
+                                <div class="property-features">
+                                    <span><i class="fa fa-home"></i> 5,000 m<sup>2</sup></span>
+                                    <span><i class="fa fa-hdd-o"></i> 2 Bed</span>
+                                    <span><i class="fa fa-male"></i> 2 Bath</span>
+                                </div>
+                                <div class="property-content">
+                                    <h3>
+                                        <a href="#">The Urban Life</a>
+                                        <small>22, JJ Road, Yogyakarta</small>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6 col-xs-6 element-item rent" data-category="rent">
+                            <div class="property-container">
+                                <div class="property-image">
+                                    <img src="assets/img/img03.jpg" alt="arillo real estate theme">
+                                    <div class="property-price">
+                                        <h4>Villa</h4>
+                                        <span>$300,000<small>/year</small></span>
+                                    </div>
+                                </div>
+                                <div class="property-features">
+                                    <span><i class="fa fa-home"></i> 5,000 m<sup>2</sup></span>
+                                    <span><i class="fa fa-hdd-o"></i> 2 Bed</span>
+                                    <span><i class="fa fa-male"></i> 2 Bath</span>
+                                </div>
+                                <div class="property-content">
+                                    <h3>
+                                        <a href="#">The Urban Life</a>
+                                        <small>22, JJ Road, Yogyakarta</small>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 col-sm-6 col-xs-6 element-item sale" data-category="sale">
+                            <div class="property-container">
+                                <div class="property-image">
+                                    <img src="assets/img/img04.jpg" alt="arillo real estate theme">
+                                    <div class="property-price">
+                                        <h4>Commercial</h4>
+                                        <span>$800,000</span>
+                                    </div>
+                                </div>
+                                <div class="property-features">
+                                    <span><i class="fa fa-home"></i> 5,000 m<sup>2</sup></span>
+                                    <span><i class="fa fa-hdd-o"></i> 2 Bed</span>
+                                    <span><i class="fa fa-male"></i> 2 Bath</span>
+                                </div>
+                                <div class="property-content">
+                                    <h3>
+                                        <a href="#">The Urban Life</a>
+                                        <small>22, JJ Road, Yogyakarta</small>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6 col-xs-6 element-item rent" data-category="rent">
+                            <div class="property-container">
+                                <div class="property-image">
+                                    <img src="assets/img/img05.jpg" alt="arillo real estate theme">
+                                    <div class="property-price">
+                                        <h4>Residential</h4>
+                                        <span>$800,000<small>/year</small></span>
+                                    </div>
+                                </div>
+                                <div class="property-features">
+                                    <span><i class="fa fa-home"></i> 5,000 m<sup>2</sup></span>
+                                    <span><i class="fa fa-hdd-o"></i> 2 Bed</span>
+                                    <span><i class="fa fa-male"></i> 2 Bath</span>
+                                </div>
+                                <div class="property-content">
+                                    <h3>
+                                        <a href="#">The Urban Life</a>
+                                        <small>22, JJ Road, Yogyakarta</small>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-md-4 col-sm-6 col-xs-6 element-item sale" data-category="sale">
+                            <div class="property-container">
+                                <div class="property-image">
+                                    <img src="assets/img/img02.jpg" alt="arillo real estate theme">
+                                    <div class="property-price">
+                                        <h4>Residential</h4>
+                                        <span>$800,000</span>
+                                    </div>
+                                </div>
+                                <div class="property-features">
+                                    <span><i class="fa fa-home"></i> 5,000 m<sup>2</sup></span>
+                                    <span><i class="fa fa-hdd-o"></i> 2 Bed</span>
+                                    <span><i class="fa fa-male"></i> 2 Bath</span>
+                                </div>
+                                <div class="property-content">
+                                    <h3>
+                                        <a href="#">The Urban Life</a>
+                                        <small>22, JJ Road, Yogyakarta</small>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4 col-sm-6 col-xs-6 element-item rent" data-category="rent">
+                            <div class="property-container">
+                                <div class="property-image">
+                                    <img src="assets/img/img03.jpg" alt="arillo real estate theme">
+                                    <div class="property-price">
+                                        <h4>Villa</h4>
+                                        <span>$300,000<small>/year</small></span>
+                                    </div>
+                                </div>
+                                <div class="property-features">
+                                    <span><i class="fa fa-home"></i> 5,000 m<sup>2</sup></span>
+                                    <span><i class="fa fa-hdd-o"></i> 2 Bed</span>
+                                    <span><i class="fa fa-male"></i> 2 Bath</span>
+                                </div>
+                                <div class="property-content">
+                                    <h3>
+                                        <a href="#">The Urban Life</a>
+                                        <small>22, JJ Road, Yogyakarta</small>
+                                    </h3>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>  <!-- isotope -->
+                </div>  <!-- row -->
+
+{{--            </div>--}}
+        </div>
+    </section>
+    <!-- begin:footer -->
+    <section id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 copyright">
+                    <p>Copyright &copy; 2015. All Right Reserved.</p>
+                    <p>Theme : Sweet<strong>Home</strong> 1.0 Designed by <strong>ThemeWagon <sup>TM</sup></strong></p>
+                    <a href="#top" class="btn btn-primary scroltop"><i class="fa fa-angle-up"></i></a>
+                </div>
+            </div>
+            <!-- end:copyright -->
+        </div>
+    </section>
+    <script src="../assets/js/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.2/modernizr.js"></script>
+    <script src="../assets/js/isotope.pkgd.min.js"></script>
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/owl.carousel.min.js"></script>
+
+
+    <script src="../assets/js/rotator.js"></script>
+
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="../assets/js/jquery.scrollTo.min.js"></script>
+    <script src="../assets/js/jquery.easing.js"></script>
+    <script src="../assets/js/jquery.backstretch.js"></script>
+    <script src="../assets/js/jquery.nicescroll.min.js"></script>
+    <script src="../assets/js/script.js"></script>
+</body>
+</html>
